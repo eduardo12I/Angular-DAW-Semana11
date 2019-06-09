d2d31d2ec6cf5229c4c2a625de7767dfb9887423
eduardo12I <14101204@ue.edu.pe>
2019-06-09 13:15:21 -0500

    Add files via upload

---
+++ src/app/producto-class.ts
@@ -0,0 +1,10 @@
+export class ProductoClass {
+    create(Constructor: typeof ProductoClass){
+      Constructor.prototype;
+    }
+
+  nombre:string;
+  precio:number;
+  descripcion:string;
+  fabricante:string;
+}
